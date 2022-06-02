9fe52d61273aafb70ded5d28b4258fc5f47f9a85
ray314 <yeungj1602@gmail.com>
2022-06-02 21:25:47 +1200

    add expressjs

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
 const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
 const { Intents, Client } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
