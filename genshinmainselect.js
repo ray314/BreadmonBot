fe742980b8d5f5ad4fd51f435244556e25c0720a
ray314 <yeungj1602@gmail.com>
2022-06-02 20:58:20 +1200

    updated

--- genshinmainselect.js
+++ genshinmainselect.js
@@ -247,7 +247,7 @@ const Khaenriah = [
 
 module.exports.Create = function (client) {
 	client.on('ready', () => {
-		client.channels.fetch('925168032274350130').then(channel => {
+		client.channels.fetch('925162470107123752').then(channel => {
 			if (channel) {
 				let EMBED = new MessageEmbed()
 					.setColor('#eb7b36')
